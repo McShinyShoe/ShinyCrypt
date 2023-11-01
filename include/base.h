fa13134840194d1d00c553a82b98b60f6dbb6255
ShinyShoe <mcshinyshoe@gmail.com>
2023-11-01 16:26:02 +0700

    feat: Added base library

---
+++ include/base.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef SHINY_BASE_LIB
+#define SHINY_BASE_LIB 1.1
+
+#include <cstddef>
+#include <unordered_map>
+#include <string>
+#include <initializer_list>
+
+namespace shiny
+{
+    class Base
+    {
+    private:
+        std::unordered_map<size_t, char> charMap;
+        std::unordered_map<char, size_t> numMap;
+        
+    public:
+        inline size_t radix() const { return charMap.size(); }
+        size_t encode(const char& chr) const;
+        size_t encode(std::string str) const;
+        const char& find(const size_t& index) const;
+        std::string decode(size_t num) const;
+        std::string decodeN(size_t num, size_t N) const;
+
+        Base() = delete;
+        Base(const Base& other);
+        Base(const char* chars, size_t radix);
+        Base(const char* chars);
+        Base(std::initializer_list<char> list);
+    };
+    extern const Base base2;
+    extern const Base base8;
+    extern const Base base10;
+    extern const Base base16;
+    extern const Base& binary;
+    extern const Base& octal;
+    extern const Base& decimal;
+    extern const Base& hexadecimal;
+}
+#endif
---
+++ src/base.cpp
@@ -0,0 +1,93 @@
+#include <cstddef>
+#include <unordered_map>
+#include <string>
+#include <initializer_list>
+#include <algorithm>
+
+#include "../include/base.h"
+
+namespace shiny {
+    size_t Base::encode(const char& chr) const {
+        if(numMap.find(chr) == numMap.end())
+            return -1;
+        return numMap.at(chr);
+    }
+
+    size_t Base::encode(std::string str) const {
+        size_t multiplier = 1;
+        size_t r = 0;
+        size_t t;
+        for(auto it = str.rbegin(); it != str.rend(); it++) {
+            if ((t = encode(*it)) == -1) return -1;
+            r += t * multiplier;
+            multiplier *= radix();
+        }
+        return r;
+    }
+
+    const char& Base::find(const size_t& index) const {
+        if(charMap.find(index) == charMap.end())
+            return EOF;
+        return charMap.at(index);
+    }
+
+    std::string Base::decode(size_t num) const {
+        if(num == 0) return std::string(1, charMap.at(0));
+        std::string r;
+        while(num) {
+            r.push_back(charMap.at(num % radix()));
+            num /= radix();
+        }
+        std::reverse(r.begin(), r.end());
+        return r;
+    }
+
+    std::string Base::decodeN(size_t num, size_t N) const {
+        std::string r;
+        while(N--) {
+            r.push_back(charMap.at(num % radix()));
+            num /= radix();
+        }
+        std::reverse(r.begin(), r.end());
+        return r;
+    }
+
+    Base::Base(const Base& other) {
+        charMap = other.charMap;
+        numMap = other.numMap;
+    };
+
+    Base::Base(const char* chars, size_t radix) {
+        while(radix--) {
+            charMap[radix] = chars[radix];
+            numMap[chars[radix]] = radix;
+        }
+    };
+
+    Base::Base(const char* chars) {
+        size_t i = 0;
+        while(chars[i]) {
+            charMap[i] = chars[i];
+            numMap[chars[i]] = i;
+            i++;
+        }
+    };
+        
+    Base::Base(std::initializer_list<char> list) {
+        size_t i = 0;
+        for(const char& chr : list) {
+            charMap[i] = chr;
+            numMap[chr] = i;
+            i++;
+        }
+    };
+
+    const Base base2("01");
+    const Base base8("01234567");
+    const Base base10("0123456789");
+    const Base base16("0123456789abcdef");
+    const Base& binary = base2;
+    const Base& octal = base8;
+    const Base& decimal = base10;
+    const Base& hexadecimal = base16;
+}

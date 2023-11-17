b2c2cc6955c7787684ff077c412fba7ab754ff25
ShinyShoe <mcshinyshoe@gmail.com>
2023-11-17 19:38:42 +0700

    feat: added SHA1 cryptographic hash function

--- include/sha.h
+++ include/sha.h
@@ -53,4 +53,61 @@ namespace shiny {
         void compute();
         std::array<uint32_t, 5> getDisgest() const;
     };
+    class SHA1
+    {
+    public:
+        inline static constexpr uint32_t stateInitial[5] = {
+            0x67452301,
+            0xefcdab89,
+            0x98badcfe,
+            0x10325476,
+            0xc3d2e1f0
+        };
+        struct {
+            inline uint32_t operator[](const size_t index) const
+            {
+                switch (index / 20)
+                {
+                case 0:
+                    return 0x5a827999;
+                case 1:
+                    return 0x6ed9eba1;
+                case 2:
+                    return 0x8f1bbcdc;
+                case 3:
+                    return 0xca62c1d6;
+                default:
+                    return 0;
+                }
+            }
+        }
+        static constexpr constant = {};
+    
+    public:
+        template<typename T>
+        inline T F(T x, T y, T z, size_t t) {
+            switch (t / 20)
+            {
+            case 0:
+                return CHOICE(x, y ,z);
+            case 1:
+                return PARITY(x, y, z);
+            case 2:
+                return MAJOR(x, y, z);
+            case 3:
+                return PARITY(x, y, z);
+            default:
+                return T();
+            }
+        }    
+    
+    private:
+        std::vector<std::array<uint32_t, 80>> blocks;
+        std::vector<std::array<uint32_t, 5>> hashes;
+
+    public:
+        void init(std::string str);
+        void compute();
+        std::array<uint32_t, 5> getDisgest() const;
+    };
 }
